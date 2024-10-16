6ae8c2474460460a4065a2034b76aeb559d3a6bb
Pierre-Alexandre Augustin <alexandre.augustin10@gmail.com>
2024-10-16 11:19:06 +0200

    Création du caroussel

---
+++ src/components/Carrousel/carrousel.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+
+function AccomodationCarrousel({ accommodationId }) {
+    const [items, setItems] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/data/accomodation.json");
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+                const data = await response.json();
+                setItems(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const selectedItem = items.find((item) => item.id === accommodationId);
+    const hasPictures = selectedItem && selectedItem.pictures && selectedItem.pictures.length > 0;
+
+    const nextPicture = () => {
+        // Passe à l'image suivante
+        setCurrentIndex((prevIndex) =>
+            prevIndex === selectedItem.pictures.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    const prevPicture = () => {
+        // Revient à l'image précédente
+        setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? selectedItem.pictures.length - 1 : prevIndex - 1
+        );
+    };
+
+    return (
+        <div className="carrousel">
+            <div className="carrousel__container">
+                <div className="carrousel__items">
+                    {hasPictures ? (
+                        <div className="carrousel__items__content">
+                            <img src={selectedItem.pictures[currentIndex]} alt={currentIndex} />
+                            <div className="carrousel__items__content__counter">
+                                <p>
+                                    {currentIndex + 1} / {selectedItem.pictures.length}
+                                </p>
+                            </div>
+                        </div>
+                    ) : (
+                        <p>Chargement...</p>
+                    )}
+                </div>
+            </div>
+
+            {hasPictures && selectedItem.pictures.length > 1 && (
+                <div className="button">
+                    <div className="button__left">
+                        <i onClick={prevPicture} className="fa-solid fa-chevron-left"></i>
+                    </div>
+                    <div className="button__right">
+                        <i onClick={nextPicture} className="fa-solid fa-chevron-right"></i>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default AccomodationCarrousel;
--- src/pages/Accomodation/accomodation.jsx
+++ src/pages/Accomodation/accomodation.jsx
@@ -1,14 +1,33 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../components/Header/header";
+import AccomodationCarrousel from "../../components/Carrousel/carrousel";
+import Footer from "../../components/Footer/footer";
 
 function Accomodation() {
+    const { id } = useParams();
     return (
         <div>
             <div className="header__main">
                 <Header />
+                <main>
+                    {/* <AccomodationCarrousel /> */}
+                    <AccomodationCarrousel accommodationId={id} />
+                </main>
             </div>
+            <Footer />
         </div>
     );
+    // return (
+    //     <div>
+    //         <div className="header__main">
+    //             <Header />
+    //             <main>
+    //                 <AccomodationCarrousel />
+    //             </main>
+    //         </div>
+    //     </div>
+    // );
 }
 
 export default Accomodation;
